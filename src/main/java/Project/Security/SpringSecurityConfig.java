3ae6c998c2cd61a79bca9ece0e61d969c23883da
binq1000 <t.daniels@student.fontys.nl>
2017-06-09 23:52:56 +0200

    Added basic authentication that uses an Owner to log in. Added a dependency for security. Added a method to the OwnerDao to look up a Owner by its username.

---
+++ src/main/java/Project/Security/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package Project.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Nekkyou on 6-6-2017.
+ */
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+					.anyRequest().fullyAuthenticated()
+					.and()
+				.httpBasic()
+					.and()
+				.csrf().disable();
+	}
+}
